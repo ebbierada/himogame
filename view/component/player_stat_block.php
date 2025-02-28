c9adf850bbb3c4098bf101c3455b1c7ebe9cc5c7
Edmund Rada <Edmund.Rada@uhs.nhs.uk>
2025-02-28 17:44:56 +0000

    Continue with Player stat

--- view/component/player_stat_block.php
+++ view/component/player_stat_block.php
@@ -1,6 +1,52 @@
-<section>
-    Class:
-    level:
-    Race:
-    experience point(XP):
+<section class="scroll">
+    <div class="stat character-info">
+        <h3>Character info</h3>
+        <dl>
+            <dt>Name</dt>
+                <dd>Zephyra Nightwhisper</dd>
+            <dt>Race</dt>
+                <dd>Tiefling</dd>
+            <dt>Class</dt>
+                <dd>Warlock</dd>
+        </dl>
+    </div>
+    <div class="stat ability-info">
+        <h3>Ability Scores / Modifier</h3>
+        <dl>
+            <dt>Strengh</dt>
+                <dd>9 / -1</dd>
+            <dt>Dexterity</dt>
+                <dd>14 / +2</dd>
+            <dt>Constitution</dt>
+                <dd>13 / +1</dd>
+            <dt>Intelligence</dt>
+                <dd>11 / +0</dd>
+            <dt>Wisdom</dt>
+                <dd>12 / +1</dd>
+            <dt>Charisma</dt>
+                <dd>17 / +4</dd>
+        </dl>
+    </div>
+    <div class="stat skill-info">
+        <h3>Skills</h3>
+        <dl>
+            <dt>Deception</dt>
+                <dd>+6</dd>
+            <dt>Persuasion</dt>
+                <dd>+6</dd>
+            <dt>Arcana</dt>
+                <dd>+2</dd>
+            <dt>Stealth</dt>
+                <dd>+2</dd>
+        </dl>
+    </div>
+    <div class="stat spell-info">
+        <h3>Spells</h3>
+        <dl>
+            <dt>Cantrips</dt>
+                <dd>Eldritch Blast</dd>
+            <dt>Spells Known</dt>
+                <dd>Charm Person, Hex</dd>
+        </dl>
+    </div>
 </section>
